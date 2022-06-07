a12279d39df861ffd0136e365e8afa9f90bad86d
Boshi <boshi.litw@gmail.com>
2022-06-07 08:37:32 +0000

    add print

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 //
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include "headers/net.h"
+#include <iostream>
 
-int main()
+static ncnn::Mat detect_yolo(const cv::Mat& img)
 {
-    cv::Mat img = cv::imread("face.jpg", cv::IMREAD_GRAYSCALE);
     int w = img.cols;
     int h = img.rows;
-
     int target_width = 640;// target resized width
     int target_height = 640;// target resized height
 
@@ -23,15 +23,111 @@ int main()
     ncnn::Net net;
     net.load_param("models/v5s-p6_wider_face_sim-opt/model.param");
     net.load_model("models/v5s-p6_wider_face_sim-opt/model.bin");
-
+    std::cout << "載入模型成功!" << "\n";
     ncnn::Extractor ex = net.create_extractor();
     ex.set_light_mode(true);
     ex.set_num_threads(4);
 
     ex.input("images", in);
 
-    ncnn::Mat feat;
-    ex.extract("output", feat);
-    printf("x = %d\n", feat.w);
+    ncnn::Mat out;
+    ex.extract("output", out);
+
+    return out;
+}
+
+void pretty_print(const ncnn::Mat& m)
+{
+    printf("pretty_print----------------\n");
+    for (int q=0; q<m.c; q++)
+    {
+        const float* ptr = m.channel(q);
+        for (int y=0; y<m.h; y++)
+        {
+            for (int x=0; x<m.w; x++)
+            {
+                printf("%f ", ptr[x]);
+            }
+            ptr += m.w;
+            printf("\n");
+        }
+        printf("------------------------\n");
+    }
+}
+
+void visualize(const char* title, const ncnn::Mat& m)
+{
+    std::vector<cv::Mat> normed_feats(m.c);
+
+    for (int i=0; i<m.c; i++)
+    {
+        cv::Mat tmp(m.h, m.w, CV_32FC1, (void*)(const float*)m.channel(i));
+
+        cv::normalize(tmp, normed_feats[i], 0, 255, cv::NORM_MINMAX, CV_8U);
+
+        cv::cvtColor(normed_feats[i], normed_feats[i], cv::COLOR_GRAY2BGR);
+
+        // check NaN
+        for (int y=0; y<m.h; y++)
+        {
+            const float* tp = tmp.ptr<float>(y);
+            uchar* sp = normed_feats[i].ptr<uchar>(y);
+            for (int x=0; x<m.w; x++)
+            {
+                float v = tp[x];
+                if (v != v)
+                {
+                    sp[0] = 0;
+                    sp[1] = 0;
+                    sp[2] = 255;
+                }
+
+                sp += 3;
+            }
+        }
+    }
+
+    int tw = m.w < 10 ? 32 : m.w < 20 ? 16 : m.w < 40 ? 8 : m.w < 80 ? 4 : m.w < 160 ? 2 : 1;
+    int th = (m.c - 1) / tw + 1;
+
+    cv::Mat show_map(m.h * th, m.w * tw, CV_8UC3);
+    show_map = cv::Scalar(127);
+
+    // tile
+    for (int i=0; i<m.c; i++)
+    {
+        int ty = i / tw;
+        int tx = i % tw;
+
+        normed_feats[i].copyTo(show_map(cv::Rect(tx * m.w, ty * m.h, m.w, m.h)));
+    }
+
+    cv::resize(show_map, show_map, cv::Size(0,0), 2, 2, cv::INTER_NEAREST);
+    cv::imshow(title, show_map);
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
+        return -1;
+    }
+
+    const char* imagepath = argv[1];
+
+    cv::Mat m = cv::imread(imagepath, 1);
+    if (m.empty())
+    {
+        fprintf(stderr, "cv::imread %s failed\n", imagepath);
+        return -1;
+    }
+    else
+        std::cout << "讀取圖片成功!" << "\n";
+
+    ncnn::Mat out = detect_yolo(m);
+    pretty_print(out);
+//    visualize("output", out);
     return 0;
 }
